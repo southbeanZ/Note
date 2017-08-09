47cf094a0a30464a3aaf3e9bba9aaa7d8b2bd698
Zero Zhang <southbean2013@gmail.com>
2017-08-09 22:09:30 +0800

    update EditItem components

--- src/components/EditItem.vue
+++ src/components/EditItem.vue
@@ -1,13 +1,13 @@
 <<template>
   <form class="m-item">
-    <label for="title">标题</label>
-    <input type="text" name="title" class="J_title" placeholder="写下笔记标题"><br>
-    <!--<label for="time">时间</label>-->
-    <!--<input type="date" name="time" class="J_time"><br>-->
-    <label for="content">内容</label>
-    <textarea name="content" cols="30" rows="10" class="J_content" placeholder="在这里输入正文"></textarea><br>
     <a href="javascript:;" class="btn btn-add" @click="addNote">添加</a>
     <!--<input type="submit" value="清空" class="btn btn-clear">-->
+    <!-- <label for="title">标题</label> -->
+    <input type="text" name="title" class="J_title" placeholder="写下笔记标题">
+    <!--<label for="time">时间</label>-->
+    <!--<input type="date" name="time" class="J_time"><br>-->
+    <!-- <label for="content">内容</label> -->
+    <textarea name="content" class="J_content" placeholder="在这里输入正文"></textarea><br>
   </form>
 </template>
 <script>
@@ -28,5 +28,22 @@ export default {
 <style lang="scss" scoped>
 .m-item {
   text-align: left;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 20px;
+  input {
+    display: block;
+    width: 100%;
+    height: 40px;
+    font-size: 24px;
+    &::-webkit-input-placeholder {
+      font-size: 24px;
+    }
+  }
+  textarea {
+    display: block;
+    width: 100%;
+    height: 300px;
+  }
 }
 </style>
