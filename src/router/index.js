312a4311e54402cf850665be5beaf1799a06cbc9
Zero Zhang <southbean2013@gmail.com>
2017-08-14 15:01:37 +0800

    separate homepage out of app

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Panel from '@/components/Panel'
+import Login from '@/pages/Login'
+import Home from '@/pages/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Panel',
-      component: Panel
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
