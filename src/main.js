a7398a3012b1f4d556da430f73f8eb61fd1a78ec
Zero Zhang <southbean2013@gmail.com>
2017-08-15 23:23:07 +0800

    install vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import './assets/style/reset.scss'
 import './assets/style/common.scss'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
