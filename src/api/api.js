52075c38979b322f6bb544cf29664c7469cff790
Zero Zhang <southbean2013@gmail.com>
2017-09-26 22:21:33 +0800

    [Update] localStorage api

--- src/api/api.js
+++ src/api/api.js
@@ -9,5 +9,23 @@ export default {
     }
     setTimeout(() => cb(notes), 100)
     // setTimeout(() => cb(data.list), 100)
+  },
+  setNoteItem (data) {
+    data.id = data.id || new Date().getTime()
+    data.summary = data.content.length > 40 ? data.content.slice(0, 40) + '...' : data.content
+    data.star = data.star || false
+    localStorage.setItem('notes_' + data.id, JSON.stringify(data))
+  },
+  removeNoteItem (id) {
+    localStorage.removeItem('notes_' + id)
+    let notes = JSON.parse(localStorage.getItem('notesNames'))
+    notes.splice(notes.indexOf('notes_' + id), 1)
+    this.setNoteNames(notes)
+  },
+  setNoteNames (data) {
+    localStorage.setItem('notesNames', JSON.stringify(data))
+  },
+  getItem (key) {
+    return JSON.parse(localStorage.getItem(key))
   }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,5 @@
 import * as types from './mutation-types'
+import api from '@/api/api'
 
 function getIndex (list, id) {
   return list.findIndex((ele) => {
@@ -13,6 +14,7 @@ const mutations = {
         title: '',
         content: '',
         tagList: [],
+        summary: '',
         star: false
       }
     } else {
@@ -22,36 +24,27 @@ const mutations = {
     state.isNew = data.isNew
   },
   [types.ADD_NOTE] (state, data) {
-    data.id = new Date().getTime()
-    data.summary = data.content.length > 40 ? data.content.slice(0, 40) + '...' : data.content
-    data.star = false
-    localStorage.setItem('notes_' + data.id, JSON.stringify(data))
-    let notes = JSON.parse(localStorage.getItem('notesNames')) || []
+    api.setNoteItem(data)
+    let notes = api.getItem('notesNames') || []
     notes.push('notes_' + data.id)
-    localStorage.setItem('notesNames', JSON.stringify(notes))
+    api.setNoteNames(notes)
     state.list = [...state.list, data]
     state.isNew = false
   },
   [types.DELETE_NOTE] (state, id) {
-    localStorage.removeItem('notes_' + id)
-    let notes = JSON.parse(localStorage.getItem('notesNames'))
-    notes.splice(notes.indexOf('notes_' + id), 1)
-    localStorage.setItem('notesNames', JSON.stringify(notes))
+    api.removeNoteItem(id)
     state.list.splice(getIndex(state.list, id), 1)
     state.activeItem = null
   },
   [types.UPDATE_NOTE] (state, newData) {
-    let _id = newData.id
-    // state.list[_id] = newData
-    let index = getIndex(state.list, _id)
-    newData.summary = newData.content.length > 40 ? newData.content.slice(0, 40) + '...' : newData.content
+    let index = getIndex(state.list, newData.id)
+    api.setNoteItem(newData)
     state.list[index] = newData
-    localStorage.setItem('notes_' + _id, JSON.stringify(newData))
   },
   [types.STAR_NOTE] (state, id) {
     let index = getIndex(state.list, id)
     state.list[index].star = !state.list[index].star
-    localStorage.setItem('notes_' + id, JSON.stringify(state.list[index]))
+    api.setNoteItem(state.list[index])
   },
   [types.GET_NOTELIST] (state, data) {
     state.list = data
@@ -71,7 +64,7 @@ const mutations = {
     tagList = state.list[index].tagList || []
     tagList.push(data)
     state.list[index].tagList = [...new Set(tagList)]
-    localStorage.setItem('notes_' + state.activeItem.id, JSON.stringify(state.list[index]))
+    api.setNoteItem(state.list[index])
   },
   [types.SHOW_TAG_LIST] (state) {
     state.showNoteList = !state.showNoteList
